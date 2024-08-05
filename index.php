cd98229907ce2f1f10efc590a3fe90ad65576860
Luciano de Jesus Sales <123966631+LutchaSales@users.noreply.github.com>
2024-08-05 17:56:30 -0300

    Atualização
    adicionando novos arquivos e códigos

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
         <meta charset="UTF-8">
         <title>MP3 em PHP</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="style.css">
+    
+    
     </head>
     <body>
         <div class="container">
             <?php
+                include_once 'helpers.php';
+                
                 if(isset($_GET['page'])){
                     if (file_exists("pages/{$_GET['page']}.php")){
                         include_once "pages/{$_GET['page']}.php";
@@ -17,7 +21,7 @@
                     }
                     
                 } else {
-                    include_once 'pages/albuns.php';
+                    include_once 'pages/albums.php';
                 }
             
             ?>
